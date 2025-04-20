d2855786506ce6c610ab774e57509c5babff4e8a
Karthik Rishinarada <karthikrk11135@gmail.com>
2025-04-20 16:27:23 +0530

    first commit

--- actions/airdrop.ts
+++ actions/airdrop.ts
@@ -1,5 +1,89 @@
-"use strict"
+'use server';
+import {
+  createTransferInstruction,
+  getOrCreateAssociatedTokenAccount,
+} from '@solana/spl-token';
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  Transaction,
+  clusterApiUrl,
+  sendAndConfirmTransaction,
+} from '@solana/web3.js';
+import { Keypair } from '@solana/web3.js';
+import bs58 from 'bs58';
+import dotenv from 'dotenv';
 
-export const airdropTokens = async (receiverAddress: string) => {
-    
+dotenv.config();
+
+const pvKeyStr = process.env.PRIVATE_KEY;
+
+if (!pvKeyStr) {
+  throw new Error('Set your env variables');
 }
+
+const pvKeyUint8 = bs58.decode(pvKeyStr as string);
+const kp = Keypair.fromSecretKey(new Uint8Array(pvKeyUint8));
+
+const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+const mintAddress = process.env.MINT_ADDRESS;
+
+if (!mintAddress) {
+  throw new Error('Set your envs right');
+}
+
+export const airdropTokens = async (
+  receiverAddress: string,
+  tokens: string
+) => {
+  const receiverPublicKey = new PublicKey(receiverAddress);
+  try {
+    const amount = (tokens as unknown as number) * LAMPORTS_PER_SOL;
+
+    const sourceAssociatedTokenAccount =
+      await getOrCreateAssociatedTokenAccount(
+        connection,
+        kp,
+        new PublicKey(mintAddress),
+        kp.publicKey
+      );
+    console.log(sourceAssociatedTokenAccount.address.toString());
+
+    const destinationAssociatedTokenAccount =
+      await getOrCreateAssociatedTokenAccount(
+        connection,
+        kp,
+        new PublicKey(mintAddress),
+        receiverPublicKey
+      );
+
+    const tx = new Transaction();
+    tx.add(
+      createTransferInstruction(
+        sourceAssociatedTokenAccount.address,
+        destinationAssociatedTokenAccount.address,
+        kp.publicKey,
+        amount
+      )
+    );
+
+    const latestBlockHash = await connection.getLatestBlockhash('confirmed');
+    tx.recentBlockhash = latestBlockHash.blockhash;
+
+    const signature = await sendAndConfirmTransaction(connection, tx, [kp]);
+    console.log('Success -> ', signature);
+
+    return {
+      message: 'Success',
+      ok: true,
+      sig: signature,
+    };
+  } catch (err) {
+    return {
+      message: 'Failure',
+      ok: false,
+      sig: '',
+    };
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,81 @@
-
+'use client';
+import { useState } from 'react';
+import { airdropTokens } from '@/actions/airdrop';
 
 export default function Home() {
+  const [userAdd, setUserAdd] = useState('');
+  const [tokens, setTokens] = useState('');
+  const [sig, setSig] = useState('');
+  const [finalLabel, setFinalLabel] = useState<number>(2);
+  const [loader, setLoader] = useState(false);
+
+  const submitHandler = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    if (!userAdd || !tokens) return;
+    setLoader(true);
+    const res = await airdropTokens(userAdd, tokens);
+    if (res.ok) {
+      setSig(res.sig);
+      setFinalLabel(1);
+    } else {
+      setFinalLabel(0);
+    }
+    setLoader(false);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <form>
-        <label></label>
+    <div className="bg-black  h-screen text-white text-center">
+      <h1 className="text-4xl text-blue-400 p-10 font-black">
+        Karthik coins free airdrop on devnet!!
+      </h1>
+      <form className="">
+        <label className="me-4">Select your address</label>
+        <input
+          onChange={(e) => setUserAdd(e.target.value)}
+          type="text"
+          className="border rounded p-2"
+        ></input>
+        <br></br>
+        <select
+          onChange={(e) => setTokens(e.target.value)}
+          className="m-3 text-2xl"
+        >
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+          <option>10</option>
+        </select>
+        <br></br>
+
+        {!loader ? (
+          <button
+            onClick={submitHandler}
+            className="px-5 py-1.5 rounded border cursor-pointer hover:-translate-y-0.5 transition-all"
+          >
+            Get tokens
+          </button>
+        ) : (
+          'loading...'
+        )}
+
+        {finalLabel == 1 && (
+          <p className="m-5 text-green-500">
+            Congrats, tokens are sent - {sig}
+          </p>
+        )}
+        {finalLabel == 0 && (
+          <p className="m-5 text-red-500">
+            Bad luck, something went wrong. prolly check your inputs
+          </p>
+        )}
       </form>
     </div>
   );
